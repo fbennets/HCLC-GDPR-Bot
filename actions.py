f3840d26f1974c28dd2f8c7c735a1cfe87f47243
elias@guerlich.de <elias@guerlich.de>
2020-11-14 15:41:10 +0100

    fix import error

--- actions.py
+++ actions.py
@@ -17,6 +17,7 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
 import io
+import os
 import weasyprint 
 
 
